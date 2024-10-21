591ddfcbc1c5c74c238fe2dc075911923c799430
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2024-10-21 10:06:02 -0300

    update

---
+++ EjercicioTipoPrueba2_006/src/Operaciones.java
@@ -0,0 +1,10 @@
+
+public interface Operaciones {
+    double IVA = .19;
+    double DESCUENTO_AUTOMOVIL = .05;
+    double DESCUENTO_MOTOCICLETA=.1;
+    
+    int totalConIVA();
+    int obtenerDescuento();
+    
+}
